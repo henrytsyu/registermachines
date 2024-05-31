420aabb2aef0c00603c00569ac962adb1a4fe813
Henry Yu <henrytsyu@gmail.com>
2024-05-31 01:40:47 +0100

    [henrytsyu] feat: run step

--- src/main/java/com/henrytsyu/registermachines/runner/RMRunner.java
+++ src/main/java/com/henrytsyu/registermachines/runner/RMRunner.java
@@ -4,16 +4,49 @@ import com.henrytsyu.registermachines.parser.RMDecrement;
 import com.henrytsyu.registermachines.parser.RMIncrement;
 import com.henrytsyu.registermachines.parser.RMInstruction;
 
-public class RMRunner {
+import java.util.HashMap;
+import java.util.Map;
 
-  public boolean runInstruction(RMInstruction instruction) {
+public class RMRunner {
+  private final Map<Integer, RMInstruction> instructionMap;
+  private int programCounter;
+  private final Map<Integer, Integer> state;
+
+  public RMRunner(Map<Integer, RMInstruction> instructionMap) {
+    this(instructionMap, new HashMap<>());
+  }
+
+  public RMRunner(Map<Integer, RMInstruction> instructionMap, Map<Integer, Integer> state) {
+    this.instructionMap = instructionMap;
+    this.state = state;
+  }
+
+  public int getProgramCounter() {
+    return programCounter;
+  }
+
+  public Map<Integer, Integer> getState() {
+    return state;
+  }
+
+  /**
+   * @return whether the register machine terminates after this step
+   */
+  public boolean runStep() {
+    RMInstruction instruction = instructionMap.get(programCounter);
     if (instruction instanceof RMIncrement increment) {
+      programCounter = increment.nextLine();
       return false;
-    } else if (instruction instanceof RMDecrement decrement) {
-      return false;
-    } else {
-      // instruction instanceof RMHalt
-      return true;
     }
+    if (instruction instanceof RMDecrement decrement) {
+      if (state.getOrDefault(decrement.registerNumber(), 0) == 0) {
+        programCounter = decrement.nextLineFail();
+      } else {
+        programCounter = decrement.nextLineSuccess();
+      }
+      return false;
+    }
+    // instruction is RMHalt
+    return true;
   }
 }
